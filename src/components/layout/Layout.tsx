fcea55efef60d618db6cc78917394507e3f84091
npkg2pkc-ux <npkg2.pkc@gmail.com>
2026-03-01 20:27:48 +0700

    Update version

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -788,7 +788,7 @@ const VersionBadge = () => {
         title="Klik untuk melihat perjalanan versi"
       >
         <History className="h-3 w-3" />
-        v3.1.2
+        v3.2.1
       </button>
       <VersionHistoryModal
         isOpen={showVersionHistory}
